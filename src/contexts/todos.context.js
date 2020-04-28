401e26c88c76e47d237321f74d11b47782bb4992
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-28 15:46:34 -0300

    Spliting into 2 Contexts to improve the performance

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -7,13 +7,14 @@ const defaultTodos = [
 ];
 
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
   const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
 
   return (
-    <TodosContext.Provider value={{ todos, dispatch }}>
-      {props.children}
+    <TodosContext.Provider value={todos}>
+      <DispatchContext value={dispatch}>{props.children}</DispatchContext>
     </TodosContext.Provider>
   );
 }
